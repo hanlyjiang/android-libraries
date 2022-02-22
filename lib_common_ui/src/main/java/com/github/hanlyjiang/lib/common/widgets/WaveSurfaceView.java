4cad0604436afa5c854e2cc6d5060b2f1cdf7968
hanlyjiang <hanlyjiang@outlook.com>
2022-02-23 00:01:17 +0800

    feature: 波浪控件

--- lib_common_ui/src/main/java/com/github/hanlyjiang/lib/common/widgets/WaveSurfaceView.java
+++ lib_common_ui/src/main/java/com/github/hanlyjiang/lib/common/widgets/WaveSurfaceView.java
@@ -5,22 +5,27 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.*;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.github.hanlyjiang.lib.common.ui.R;
 
-public class WaveView extends View {
+public class WaveSurfaceView extends SurfaceView {
 
-    private static final String TAG = WaveView.class.getSimpleName();
+    private static final String TAG = WaveSurfaceView.class.getSimpleName();
     private int waveColor;
     private int waveBgColor;
     private int waveColumnCount;
     private int waveAnimationDuration;
-    private int totalHeight, aHeight;
-    private int totalWidth, aWidth;
+    private int aHeight;
+    private int aWidth;
     private Paint mPaint;
     private Path mPath;
     private float[] waveXIndex;
@@ -28,24 +33,57 @@ public class WaveView extends View {
     private ObjectAnimator phaseAnimator;
     private final LinearInterpolator linearInterpolator = new LinearInterpolator();
     private float columnWidth;
+    private SurfaceHolder mHolder;
+
+    private HandlerThread mDrawHandlerThread;
+    private Handler mDrawHandler;
 
     private Rect mBgRect = new Rect();
 
-    public WaveView(Context context) {
+    private int mSurfaceHeight;
+    private int mSurfaceWidth;
+
+    SurfaceHolder.Callback mSHCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            mHolder = holder;
+            mDrawHandlerThread = new HandlerThread("waveDrawThread");
+            mDrawHandlerThread.start();
+            mDrawHandler = new Handler(mDrawHandlerThread.getLooper());
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            mSurfaceWidth = width;
+            mSurfaceHeight = height;
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+            mHolder = null;
+            if (mDrawHandlerThread != null) {
+                mDrawHandlerThread.quitSafely();
+            }
+            mDrawHandlerThread = null;
+            mDrawHandler = null;
+        }
+    };
+
+    public WaveSurfaceView(Context context) {
         super(context);
     }
 
-    public WaveView(Context context, @Nullable AttributeSet attrs) {
+    public WaveSurfaceView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs, 0, 0);
     }
 
-    public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public WaveSurfaceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs, defStyleAttr, 0);
     }
 
-    public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public WaveSurfaceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init(context, attrs, defStyleAttr, defStyleRes);
     }
@@ -57,16 +95,20 @@ public class WaveView extends View {
         if (ts.hasValue(R.styleable.WaveView_waveBackgroundColor)) {
             waveBgColor = ts.getColor(R.styleable.WaveView_waveBackgroundColor, Color.WHITE);
         }
-        waveAnimationDuration = ts.getInteger(R.styleable.WaveView_waveAnimationDuration, 600);
+        waveAnimationDuration = ts.getInteger(R.styleable.WaveView_waveAnimationDuration, 1000);
         ts.recycle();
 
         mPaint = new Paint();
         mPath = new Path();
-//        mPath.setFillType(Path.FillType.EVEN_ODD);
         mPaint.setAntiAlias(true);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setColor(waveColor);
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        getHolder().addCallback(mSHCallback);
+        setZOrderOnTop(true);
+        // 设置画布 背景透明
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
     }
 
     @Override
@@ -74,26 +116,23 @@ public class WaveView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         aWidth = MeasureSpec.getSize(widthMeasureSpec) - getPaddingLeft() - getPaddingRight();
         aHeight = MeasureSpec.getSize(heightMeasureSpec) - getPaddingTop() - getPaddingBottom();
-        totalWidth = MeasureSpec.getSize(widthMeasureSpec);
-        totalHeight = MeasureSpec.getSize(heightMeasureSpec);
         mBgRect.set(getPaddingLeft(), getPaddingTop(),
-                totalWidth - getPaddingRight(),
-                totalHeight - getPaddingBottom());
-        calcDrawInfo(aHeight, aWidth);
+                MeasureSpec.getSize(widthMeasureSpec) - getPaddingRight(),
+                MeasureSpec.getSize(heightMeasureSpec) - getPaddingBottom());
+        calcDrawInfo();
         if (phaseAnimator != null) {
             phaseAnimator.cancel();
         }
         phaseAnimator = ObjectAnimator.ofInt(this, "phase", 0, 180);
-        phaseAnimator.setDuration(2000);
+        phaseAnimator.setDuration(waveAnimationDuration);
         phaseAnimator.setRepeatCount(ValueAnimator.INFINITE);
         phaseAnimator.setRepeatMode(ValueAnimator.RESTART);
         phaseAnimator.setInterpolator(linearInterpolator);
         phaseAnimator.start();
     }
 
-    private void calcDrawInfo(int height, int width) {
-        Log.d(TAG, String.format("totalHeight=%d,totalWidth=%d", height, width));
-        columnWidth = width / (waveColumnCount * 2f - 1);
+    private void calcDrawInfo() {
+        columnWidth = aWidth / (waveColumnCount * 2f - 1);
         mPaint.setStrokeWidth(columnWidth);
         // 圆角
         aHeight = (int) (aHeight - columnWidth);
@@ -105,10 +144,34 @@ public class WaveView extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        int A = aHeight / 2;
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        if (phaseAnimator != null) {
+            if (visibility == View.VISIBLE) {
+                phaseAnimator.start();
+            } else {
+                phaseAnimator.pause();
+            }
+        }
+    }
+
+    private void doDraw() {
+        Log.e(TAG, "doDraw:" + Thread.currentThread().getName());
+        if (mHolder == null) {
+            return;
+        }
+        Canvas canvas;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            canvas = mHolder.lockHardwareCanvas();
+        } else {
+            canvas = mHolder.lockCanvas();
+        }
+        if (canvas == null) {
+            return;
+        }
+        int A = aHeight >> 1;
         float w = 1;
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
         if (waveBgColor != 0 && !mBgRect.isEmpty()) {
             mPaint.setColor(waveBgColor);
             mPaint.setStrokeWidth(0);
@@ -120,13 +183,13 @@ public class WaveView extends View {
         canvas.translate(getPaddingLeft(), getPaddingTop() + (columnWidth / 2));
         for (float x : waveXIndex) {
             float y = (int) sin(A, w, x, phase, 0);
-            Log.d(TAG, String.format("%.2f,%.2f", x, y));
             mPath.reset();
             mPath.moveTo(x, A - y);
             mPath.lineTo(x, A + y);
             canvas.drawPath(mPath, mPaint);
         }
         canvas.restore();
+        mHolder.unlockCanvasAndPost(canvas);
     }
 
     /**
@@ -150,6 +213,8 @@ public class WaveView extends View {
 
     public void setPhase(int phase) {
         this.phase = phase;
-        invalidate();
+        if (mDrawHandler != null) {
+            mDrawHandler.post(this::doDraw);
+        }
     }
 }
